5f964918f471f4dac08d31ac7619bc3a6c7fd915
icetea28 <icetea28@users.noreply.github.com>
2023-10-20 22:42:51 +0200

    added some design and savestates in dropdown

--- scripts/mainpage.js
+++ scripts/mainpage.js
@@ -1,11 +1,27 @@
+// Retrieve and set the saved selections on page load
+window.onload = function () {
+    var savedMode = localStorage.getItem('savedMode');
+    var savedGame = localStorage.getItem('savedGame');
+    var savedDifficulty = localStorage.getItem('savedDifficulty');
+
+    if (savedMode && savedGame && savedDifficulty) {
+        document.getElementById('mode').value = savedMode;
+        document.getElementById('game').value = savedGame;
+        document.getElementById('difficulty').value = savedDifficulty;
+    }
+}
+
 function confirmChoice() {
     var mode = document.getElementById("mode").value;
-    var game = document.getElementById("game").value; console.log('Game is',game);
+    var game = document.getElementById("game").value;
     var difficulty = document.getElementById("difficulty").value;
 
-    // Add a forward slash between "scripts/html" and the rest of the file path
-    var url = "scripts/html/" + mode + "-" + game + "-" + difficulty + ".html";
+    // Save the selections in localStorage
+    localStorage.setItem('savedMode', mode);
+    localStorage.setItem('savedGame', game);
+    localStorage.setItem('savedDifficulty', difficulty);
 
-    // Redirect to the selected page
+    // Construct the URL and redirect to the selected page
+    var url = "scripts/html/" + mode + "-" + game + "-" + difficulty + ".html";
     window.location.href = url;
 }
